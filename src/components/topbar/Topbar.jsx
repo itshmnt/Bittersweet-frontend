40f7cf6246d8320347593809d0f7dfaf5829219b
itshmnt <hemantxs5@gmail.com>
2021-12-21 12:53:05 +0530

    Adding Topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,39 @@
+import { Chat, NotificationAdd, Notifications, Person, Search } from "@mui/icons-material"
+import "./topbar.css"
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">Bittersweet</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="search for friends/posts/videos" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbsrLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+import Topbar from "../../components/topbar/Topbar";
+
+function Home() {
+    return (
+        <div>
+            <Topbar />
+        </div>
+    )
+}
+
+export default Home
