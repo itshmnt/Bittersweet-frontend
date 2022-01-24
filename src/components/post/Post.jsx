77842e96f58a7ae2d3a90beab480809d312acd8c
itshmnt <hemantxs5@gmail.com>
2022-01-24 16:03:45 +0530

    Like and Dislike fuctionality

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,16 +1,22 @@
 import { MoreVert } from "@mui/icons-material";
 import "./post.css";
 // import { Users } from '../../dummyData';
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { format } from 'timeago.js';
 import  { Link } from "react-router-dom";
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Post({post}) {
     const [like, setLike] = useState(post.likes.length);
     const [isLiked, setIsLiked] = useState(false);
     const [user, setUser] = useState({})
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const { user: currentUser } = useContext(AuthContext);
+
+    useEffect(()=> {
+        setIsLiked(post.likes.includes(currentUser._id))
+    },[currentUser._id, post.likes]);
 
     useEffect(()=>{
         const fetchUser = async () => {
@@ -21,6 +27,11 @@ export default function Post({post}) {
     },[post.userId])
 
     const likeHandler = () => {
+        try {
+            axios.put("/posts/" + post._id + "/like", { userId: currentUser._id});
+        } catch(err) {
+            alert(err);
+        }
         setLike(isLiked ? like-1 : like+1);
         setIsLiked(!isLiked);
     }
